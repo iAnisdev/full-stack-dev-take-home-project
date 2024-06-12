6587824e73e753590ab4a726e261b34199160e9a
Anis <imarahmandev@gmail.com>
2024-06-12 18:00:01 +0500

    Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	$: year = new Date().getFullYear();
+</script>
+
+<footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white text-black">
+	<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+		<span class="block text-sm text-black sm:text-center">&copy; {year} Web Traffics Inc. All rights reserved.</span>
+	</div>
+</footer>
