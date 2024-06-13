86859b4f8f17674e60d9b39b8c485c07f1cfc9a8
Anis <imarahmandev@gmail.com>
2024-06-13 16:51:36 +0500

    Basic UI Setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,85 @@
 <script lang="ts">
-	import { applyAction, enhance } from '$app/forms';
-	export let form;
+	interface DateRange {
+		start: Date;
+		end: Date;
+	}
 
-	let formIsSubmitting = false;
+	interface PickerOption {
+		label: string;
+		key: string;
+		value: DateRange;
+	}
+
+	let datePickerOptions: PickerOption[] = [
+		{
+			label: 'Last week',
+			key: 'last_week',
+			value: {
+				start: new Date(new Date().setDate(new Date().getDate() - 7)),
+				end: new Date()
+			}
+		},
+		{
+			label: 'Last two weeks',
+			key: 'last_two_weeks',
+			value: {
+				start: new Date(new Date().setDate(new Date().getDate() - 14)),
+				end: new Date()
+			}
+		},
+		{
+			label: 'Last month',
+			key: 'last_month',
+			value: {
+				start: new Date(new Date().setDate(new Date().getDate() - 30)),
+				end: new Date()
+			}
+		},
+		{
+			label: 'Last quarter',
+			key: 'last_quarter',
+			value: {
+				start: new Date(new Date().setMonth(new Date().getMonth() - 4)),
+				end: new Date()
+			}
+		},
+		{
+			label: 'Last year',
+			key: 'last_year',
+			value: {
+				start: new Date(new Date().setFullYear(new Date().getFullYear() - 1)),
+				end: new Date()
+			}
+		}
+	];
+
+	let selectedDateRange: string = datePickerOptions[0].key;
+
+	function handleDateChange(e: Event) {
+		const target = e.target as HTMLSelectElement;
+		const selectedOption = datePickerOptions.find((option) => option.key === target.value);
+		selectedDateRange = selectedOption?.key || '';
+	}
 </script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
-<main class="flex flex-col items-center justify-center w-screen h-screen bg-gray-50">
-	<div class="flex flex-col gap-4 px-10 py-4 bg-white border rounded-lg">
-		{#if formIsSubmitting}
-			<p>Submitting...</p>
-		{:else if form?.error}
-			<div class="text-red-500 ">
-				<p>{form?.error}</p>
+<main class="py-4 h-full w-full bg-[#F4F4F4]">
+	<div class="flex flex-wrap justify-start items-center mx-auto max-w-screen-xl py-8">
+		<div class="flex justify-between w-full py-8">
+			<div>
+				<p class="text-black text-2xl font-semibold">Web Traffics</p>
 			</div>
-		{:else if form?.message}
-			<p>{form.message}</p>
-		{/if}
-
-		<form
-			action="/"
-			method="POST"
-			class="flex flex-col gap-4 text-sm"
-			use:enhance={() => {
-				formIsSubmitting = true;
-
-				return async ({ result }) => {
-					formIsSubmitting = false;
-					await applyAction(result);
-				};
-			}}
-		>
-			<input
-				type="name"
-				name="name"
-				placeholder="Enter your name"
-				class="px-4 py-1 border rounded-lg w-80"
-				disabled={formIsSubmitting}
-			/>
-
-			{#if form?.errors?.name}
-				<small class="text-xs text-red-500">{form?.errors?.name[0]}</small>
-			{/if}
-			<button
-				type="submit"
-				disabled={formIsSubmitting}
-				class="px-4 py-2 text-white rounded-lg bg-slate-800">Submit</button
-			>
-		</form>
+			<div>
+				<select
+					id="large"
+					class="block w-full px-2 py-2 text-base text-gray-900 border divide-y divide-gray-100 border-gray-300 rounded-lg"
+					bind:value={selectedDateRange}
+					on:change={handleDateChange}
+				>
+					{#each datePickerOptions as option}
+						<option value={option.key}>Show me {option.label}</option>
+					{/each}
+				</select>
+			</div>
+		</div>
 	</div>
 </main>
