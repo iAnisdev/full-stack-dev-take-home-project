f0b10576a6851f3061241dafeb795549930d2bc4
Anis <imarahmandev@gmail.com>
2024-06-13 19:37:14 +0500

    Clean up

--- src/server/appRouter.ts
+++ src/server/appRouter.ts
@@ -1,8 +1,6 @@
-import { helloRouter } from './routers/hello.router';
 import { createTRPCRouter } from './trpcContext';
 
 export const appRouter = createTRPCRouter({
-	hello: helloRouter
 });
 
 export type AppRouter = typeof appRouter;
